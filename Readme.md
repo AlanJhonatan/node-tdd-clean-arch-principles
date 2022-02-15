40d66bf67213998a607966de61c9a47d5ca184e6
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-02-14 22:36:58 -0300

    test: CheckLastEventStatus testing implementation

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# ✔ TDD - NodeJS, Jest & Typescript
+
+# 📃 Principles
+
+## 🤢 Anti-Patterns & Code Smells
+
+- Speculative Generality
+- God Class
+- Divergent Change
+- Blank Lines
+- High Coupling
+- Improper Instantiation _(It's not a literary term)_
+- Code Testing in Production _(It's not a literary term)_
+
+## 🤗 Design Patterns & Principles & Conventions
+
+- (YAGNI) You Ain't Gonna Need It
+- (SRP) Single Responsability
+- (AAA) Arrange, Act, Assert
+- (DI) Dependency Injection
+
+# 🧐 Principles to check
+
+- Value Objects
+- Repository
+- Red/Green Refactor
---
+++ tests/check-last-event-status.spec.ts
@@ -0,0 +1,27 @@
+class CheckLastEventStatus {
+  constructor(
+    private readonly loadLastEventRepository: LoadLastEventRepository
+  ) {}
+
+  async perform(groupId: string): Promise<void> {
+    this.loadLastEventRepository.groupId = groupId;
+  }
+}
+
+class LoadLastEventRepository {
+  groupId?: string;
+}
+
+describe('CheckLastEventStatus', () => {
+  it('should get last event data', async () => {
+    const loadLastEventRepository = new LoadLastEventRepository();
+    const checkLastEventStatus = new CheckLastEventStatus(
+      loadLastEventRepository
+    );
+
+    //class method can be either execute/perform
+    await checkLastEventStatus.perform('any_group_id');
+
+    expect(loadLastEventRepository.groupId).toBe('any_group_id');
+  });
+});

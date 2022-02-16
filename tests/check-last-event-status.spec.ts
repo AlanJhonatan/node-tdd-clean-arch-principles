dee1b42ef04cda3b9a3e7921a004335ad3a475b7
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-02-15 21:06:57 -0300

    test: ensure CheckLastEventStatus return status active when before end time

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,25 +1,33 @@
+import { reset, set } from 'mockdate';
+
 class CheckLastEventStatus {
   constructor(
     private readonly loadLastEventRepository: LoadLastEventRepository
   ) {}
 
-  async perform(groupId: string): Promise<string> {
-    await this.loadLastEventRepository.loadLastEvent(groupId);
+  async perform({ groupId }: { groupId: string }): Promise<string> {
+    const event = await this.loadLastEventRepository.loadLastEvent({ groupId });
 
-    return 'done';
+    return event === undefined ? 'done' : 'active';
   }
 }
 
 interface LoadLastEventRepository {
-  loadLastEvent: (groupId: string) => Promise<undefined>;
+  loadLastEvent: (input: {
+    groupId: string;
+  }) => Promise<{ endDate: Date } | undefined>;
 }
 
 class LoadLastEventRepositorySpy implements LoadLastEventRepository {
   groupId?: string;
-  output: undefined;
   callsCount = 0;
+  output: { endDate: Date } | undefined;
 
-  async loadLastEvent(groupId: string): Promise<undefined> {
+  async loadLastEvent({
+    groupId,
+  }: {
+    groupId: string;
+  }): Promise<{ endDate: Date } | undefined> {
     this.groupId = groupId;
     this.callsCount++;
 
@@ -43,24 +51,45 @@ const makeSut = (): SutOutput => {
 };
 
 describe('CheckLastEventStatus', () => {
+  const groupId: string = 'any_group_id';
+  beforeAll(() => {
+    set(new Date());
+  });
+
+  afterAll(() => {
+    reset();
+  });
+
   it('should get last event data', async () => {
     //class method can be either execute/perform
     const { sut, loadLastEventRepository } = makeSut();
-    await sut.perform('any_group_id');
+    await sut.perform({ groupId });
 
-    expect(loadLastEventRepository.groupId).toBe('any_group_id');
+    expect(loadLastEventRepository.groupId).toBe(groupId);
     expect(loadLastEventRepository.callsCount).toBe(1);
   });
 
   it('should return status done when group has no event', async () => {
-    const loadLastEventRepository = new LoadLastEventRepositorySpy();
+    const { sut, loadLastEventRepository } = makeSut();
     loadLastEventRepository.output = undefined;
+
+    //class method can be either execute/perform
+    const status = await sut.perform({ groupId });
+
+    expect(status).toBe('done');
+  });
+
+  it('should return status active when now is before event end time', async () => {
+    const loadLastEventRepository = new LoadLastEventRepositorySpy();
+    loadLastEventRepository.output = {
+      endDate: new Date(new Date().getTime() + 1),
+    };
 
     const sut = new CheckLastEventStatus(loadLastEventRepository);
 
     //class method can be either execute/perform
-    const status = await sut.perform('any_group_id');
+    const status = await sut.perform({ groupId });
 
-    expect(status).toBe('done');
+    expect(status).toBe('active');
   });
 });

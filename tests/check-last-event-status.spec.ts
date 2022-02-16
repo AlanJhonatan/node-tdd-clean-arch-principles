297a68e5df5cc3ec8a7e1c136dd5eef432b2f6b6
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-02-15 21:59:43 -0300

    test: ensure CheckLastEventStatus return status inReview when now is after to review time

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -12,7 +12,11 @@ class CheckLastEventStatus {
     if (event === undefined) return { status: 'done' };
 
     const now = new Date();
-    return event.endDate >= now ? { status: 'active' } : { status: 'inReview' };
+    if (event.endDate >= now) return { status: 'active' };
+
+    const reviewDurationInMs = event.reviewDurationInHours * 60 * 60 * 1000;
+    const reviewDate = new Date(event.endDate.getTime() + reviewDurationInMs);
+    return reviewDate >= now ? { status: 'inReview' } : { status: 'done' };
   }
 }
 
@@ -64,6 +68,16 @@ class LoadLastEventRepositorySpy implements LoadLastEventRepository {
 
     this.output = {
       endDate: new Date(new Date().getTime() - reviewDurationInMs),
+      reviewDurationInHours,
+    };
+  }
+
+  setEndDateAfterReviewDate(): void {
+    const reviewDurationInHours = 1;
+    const reviewDurationInMs = 1 * 60 * 60 * 1000;
+
+    this.output = {
+      endDate: new Date(new Date().getTime() - reviewDurationInMs - 1),
       reviewDurationInHours,
     };
   }
@@ -170,5 +184,15 @@ describe('CheckLastEventStatus', () => {
     const eventStatus = await sut.perform({ groupId });
 
     expect(eventStatus.status).toBe('inReview');
+  });
+
+  it('should return status done when now is after to review time', async () => {
+    const { sut, loadLastEventRepository } = makeSut();
+
+    loadLastEventRepository.setEndDateAfterReviewDate();
+
+    const eventStatus = await sut.perform({ groupId });
+
+    expect(eventStatus.status).toBe('done');
   });
 });
